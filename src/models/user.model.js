0c7b7854c1cbab118b9c89c08fd0fa6ccd81b3cb
Chetan Singh <csingh8299@gmail.com>
2024-10-27 12:23:23 +0530

    minor fix

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -39,7 +39,7 @@ const userSchema = new mongoose.Schema(
     previousBookings: [
       {
         type: mongoose.Schema.Types.ObjectId,
-        generateAccessToken: "Order",
+        ref: "Order",
       },
     ],
     myCreatedPlaces: [
