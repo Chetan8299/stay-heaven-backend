f1e1adab00f37eab0ef69e60952f9bfc947db2ff
Chetan Singh <csingh8299@gmail.com>
2024-11-01 15:56:19 +0530

    test fix

--- src/app.js
+++ src/app.js
@@ -8,16 +8,6 @@ import session from "express-session";
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin:  [
-      process.env.CORS_ORIGIN,
-      "https://api.razorpay.com"
-    ],
-    credentials: true,
-  },
-});
-
 app.use(
   cors({
     origin:  [
@@ -28,6 +18,15 @@ app.use(
   })
 );
 
+const io = new Server(server, {
+  cors: {
+    origin:  [
+      process.env.CORS_ORIGIN,
+      "https://api.razorpay.com"
+    ],
+    credentials: true,
+  },
+});
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
