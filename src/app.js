e86f45d312f2e876fb7f3232d61d42d3b89b1852
Chetan Singh <csingh8299@gmail.com>
2024-11-04 12:32:11 +0530

    rate limiting added

--- src/app.js
+++ src/app.js
@@ -3,7 +3,13 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import http, { get } from "http";
 import { Server } from "socket.io";
+import rateLimit from 'express-rate-limit';
 
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+  message: 'Too many requests, please try again later.',
+});
 
 const app = express();
 const server = http.createServer(app);
@@ -26,6 +32,7 @@ app.use(
     credentials: true,
   })
 );
+app.use(limiter);
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
